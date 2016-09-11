8075908374dac8d89e505c97841223872d974ae5
Reggie Valdez <affairsreggievaldez@gmail.com>
2016-09-11 18:45:08 +0800

    Added description

--- simple_transfer/__openerp__.py
+++ simple_transfer/__openerp__.py
@@ -1,7 +1,23 @@
 {
 	'name': 'Simple Transfer',
-	'version': '2.0',
+	'version': '1.0',
+    'category':'Warehouse',
 	'description': """
+    
+    
+    Transfer stocks in Odoo the easiest way:
+        1.) Create an easy transfer form.
+        2.) Specify the source warehouse and destination warehouse.
+        3.) Specify the products and the quantities.
+        4.) Click the transfer button.
+        
+    During the process above the module automatically:
+        * Checked the availability of stocks in the source warehouse
+        * If available, the stocks are immediately reserved
+        * The stock moves are automatically processed and transferred
+        
+    
+    
 	""",
 	'author': 'Reggie Valdez',
 	'website': '',
